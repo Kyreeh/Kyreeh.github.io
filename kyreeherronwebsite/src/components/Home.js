26a614c2be15f77ee48155d8722030c388c88bfe
Kyree <kyreeh@Clemson.edu>
2023-10-05 12:13:09 -0400

    moving index

--- kyreeherronwebsite/src/components/Home.js
+++ kyreeherronwebsite/src/components/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './StyleSheet.module.css';
 
 function Home() {
   return (
-    <div>
+    <div  className={styles.home}>
       <h1>Home Page</h1>
       {/* Add content for your home page */}
     </div>
