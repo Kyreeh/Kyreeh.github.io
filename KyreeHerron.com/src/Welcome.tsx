6f6cd5f5e6d752c70d33823f13efa5a9d3e0eb2c
Kyree <kyreeh@Clemson.edu>
2025-11-26 04:17:35 -0500

    Mobile Optimization

--- KyreeHerron.com/src/Welcome.tsx
+++ KyreeHerron.com/src/Welcome.tsx
@@ -1,20 +1,26 @@
 import PageList from "./PageList";
 import "./App.css";
-import headshot from "./assets/headshot.jpg"; // Ensure you placed this file here
-import tigerLogo from "./assets/Clemson.png"; // or .jpg, .svg, etc.
-
+import headshot from "./assets/headshot.jpg";
+import tigerLogo from "./assets/Clemson.png";
 
 const Welcome = () => {
   return (
-    <div className="welcome-fullscreen" style={{ "--welcome-bg": `url(${tigerLogo})` } as React.CSSProperties}>
-      
+    <div
+      className="welcome-fullscreen"
+      style={{ "--welcome-bg": `url(${tigerLogo})` } as React.CSSProperties}
+    >
       <div className="welcome-content">
 
+        {/* Header Section */}
         <div className="welcome-header">
-          <img src={headshot} className="welcome-image" />
-          <h1 className="welcome-title">Welcome to KyreeHerron.com</h1>
+          <img src={headshot} className="welcome-image" alt="Headshot" />
+
+          <h1 className="welcome-title">
+            Welcome to KyreeHerron.com
+          </h1>
         </div>
 
+        {/* Page Navigation Tiles */}
         <section className="welcome-list">
           <PageList />
         </section>
@@ -23,4 +29,5 @@ const Welcome = () => {
     </div>
   );
 };
+
 export default Welcome;
